e0c1c3da3001f39aa994aa5eca2d0973acf043c7
mahi1997 <mahendrakumar@Mahendras-MacBook-Air.local>
2022-09-24 00:07:30 +0530

    changes

--- Creational/src/test/java/org/example/builder/DesktopUserTest.java
+++ Creational/src/test/java/org/example/builder/DesktopUserTest.java
@@ -2,6 +2,11 @@ package org.example.builder;
 
 import org.junit.Test;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Set;
+import java.util.TreeSet;
+
 import static org.junit.Assert.*;
 
 public class DesktopUserTest {
@@ -13,5 +18,16 @@ public class DesktopUserTest {
                 .build();
         System.out.println(desktopUser.getName());
         assert desktopUser.getName().equals("mahendra");
+
+        Set<Integer> set = new TreeSet<>((a,b)->b.compareTo(a));
+        set.add(20);
+        set.add(40);
+        set.add(30);
+        set.add(10);
+        set.remove(90);
+        System.out.println(set.iterator().next());
+
+
+
     }
 }
